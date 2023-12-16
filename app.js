a3a3ff5b7bb1e215c262d97eb287d030e627f0b2
cakezero <sareahj.com@gmail.com>
2023-12-16 18:12:25 +0100

    Second Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+let correct = 0
+let failed = 0
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.post('/question', (req, res) => {
+    const data = req.body
+    if (data.q1 == 4) {
+        correct += 1
+    } else {
+        failed += 1
+    }
+    if (data.q2 == 4) {
+        correct += 1
+    } else {
+        failed += 1
+    }
+    if (data.q3 == 2) {
+        correct += 1
+    } else {
+        failed += 1
+    }
+    if (data.q4 == 7) {
+        correct += 1
+    } else {
+        failed += 1
+    }
+    res.status(200).json({ correct, failed })
+})
+
+app.listen(3000, console.log('connected to port 3000'))
